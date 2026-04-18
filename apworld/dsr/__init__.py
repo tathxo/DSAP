e4900d5c22be97363f30f11c7b4d2e226dd89e6c
tathxo <200347824+tathxo@users.noreply.github.com>
2026-04-17 19:02:25 -0500

    Fix Logic: Hellkite Bridge -> Upper UB ladder connection added

--- apworld/dsr/__init__.py
+++ apworld/dsr/__init__.py
@@ -289,6 +289,7 @@ class DSRWorld(World):
         create_connection_2way("Upper Undead Burg - Before Fog", "Upper Undead Burg - Fog")
         create_connection_2way("Upper Undead Burg - Fog", "Upper Undead Burg")
         create_connection("Upper Undead Burg - Hellkite Bridge", "Undead Burg Basement Door")
+        create_connection("Upper Undead Burg - Hellkite Bridge", "Upper Undead Burg") # Bonfire ladder
         create_connection("Upper Undead Burg", "Upper Undead Burg - Taurus Demon")
         create_connection("Upper Undead Burg - Taurus Demon", "Upper Undead Burg - Hellkite Bridge")
         create_connection_2way("Upper Undead Burg - Hellkite Bridge", "Undead Parish - Before Fog")

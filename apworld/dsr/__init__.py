20ce23ca358917ba74575be2bdb779976ac95888
matejpopda <20053580+matejpopda@users.noreply.github.com>
2026-05-24 00:26:28 +0200

    commented line

--- apworld/dsr/__init__.py
+++ apworld/dsr/__init__.py
@@ -937,8 +937,8 @@ class DSRWorld(World):
         # for debugging purposes, you may want to visualize the layout of your world. Uncomment the following code to
         # write a PlantUML diagram to the file "my_world.puml" that can help you see whether your regions and locations
         # are connected and placed as desired
-        from Utils import visualize_regions
-        visualize_regions(self.multiworld.get_region("Menu", self.player), "my_world.puml")
+        # from Utils import visualize_regions
+        # visualize_regions(self.multiworld.get_region("Menu", self.player), "my_world.puml")
  
         
     def fill_slot_data(self) -> Dict[str, object]:
